3087de4c8efa55cdf2428f656c2ae3768b4467d3
hagio <hagio@rrr-z.co.jp>
2018-01-29 15:10:12 +0900

    商品参照API追加

---
+++ src/YShoppingClient/Operations/GetItem.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rrrz\YShoppingClient\Operations;
+
+use Rrrz\YShoppingClient\Operation;
+
+class GetItem extends Operation
+{
+    protected $item_code;
+
+    function setItemCode($item_code)
+    {
+        $this->item_code = $item_code;
+        return $this;
+    }
+
+    function getUrl()
+    {
+        return $this->getBaseUrl() . '/getItem?' . http_build_query([
+                'seller_id' => $this->getSellerId(),
+                'item_code' => $this->item_code
+            ]);;
+    }
+
+    function getMethod()
+    {
+        return Operation::METHOD_GET;
+    }
+
+    function needsAuth()
+    {
+        return true;
+    }
+
+}
